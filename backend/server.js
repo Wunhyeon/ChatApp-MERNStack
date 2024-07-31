e3b01591cf84cf22a5b6c2cd60ee85623375edaf
Jaehyeon <xhwogusxh@gmail.com>
2024-07-31 14:28:44 +0900

    1.createServer

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("API is Running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  //   console.log(req.params.id);
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, () => {
+  console.log("Server Started on PORT : ", PORT);
+});
